e49ed3c8a57720d6fd624fed781953143f41faf9
nandhananm7 <nandhanalohithaksh@gmail.com>
2025-01-23 00:01:32 +0800

    Add Level 0

---
+++ src/main/java/Talko.java
@@ -0,0 +1,8 @@
+public class Talko {
+    public static void main(String[] args) {
+        String name = "Talko";
+        System.out.println("Hello you can call me" + name);
+        System.out.println("How can I help you today?");
+        System.out.println("Bye! Have a great day");
+    }
+}
