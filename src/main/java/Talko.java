3ff2e96e846ab73e319f378af387e1919be08b01
nandhananm7 <nandhanalohithaksh@gmail.com>
2025-01-31 10:12:00 +0800

    Create ASCII art for chatbot Add echo

--- src/main/java/Talko.java
+++ src/main/java/Talko.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Talko {
     public static void main(String[] args) {
-        String name = "Talko";
-        System.out.println("Hello you can call me" + name);
+        Scanner in = new Scanner(System.in);
+        String name = "talkO";
+        String symbol = "████████╗ █████╗ ██╗     ██╗  ██╗ ██████╗ \n" +
+                "╚══██╔══╝██╔══██╗██║     ██║ ██╔╝██╔═══██╗\n" +
+                "   ██║   ███████║██║     █████╔╝ ██║   ██║\n" +
+                "   ██║   ██╔══██║██║     ██╔═██╗ ██║   ██║\n" +
+                "   ██║   ██║  ██║███████╗██║  ██╗╚██████╔╝\n" +
+                "   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ";
+        String line = "----------------------------------------------";
+        System.out.println("Hello you can call me");
+        System.out.println(symbol);
         System.out.println("How can I help you today?");
+        System.out.println(line);
+        String input = in.nextLine();
+
+        while(!input.equalsIgnoreCase("bye")) {
+            System.out.println(input);
+            System.out.println(line);
+            input = in.nextLine();
+        }
         System.out.println("Bye! Have a great day");
     }
 }
