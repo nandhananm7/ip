11d77248cdc89666e751199b3b62bc977421f921
nandhananm7 <nandhanalohithaksh@gmail.com>
2025-01-31 10:46:54 +0800

    Change talkO symbol Add feature to create tasks Add "List" to display tasks

--- src/main/java/Talko.java
+++ src/main/java/Talko.java
@@ -4,21 +4,35 @@ public class Talko {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String name = "talkO";
-        String symbol = "████████╗ █████╗ ██╗     ██╗  ██╗ ██████╗ \n" +
-                "╚══██╔══╝██╔══██╗██║     ██║ ██╔╝██╔═══██╗\n" +
-                "   ██║   ███████║██║     █████╔╝ ██║   ██║\n" +
-                "   ██║   ██╔══██║██║     ██╔═██╗ ██║   ██║\n" +
-                "   ██║   ██║  ██║███████╗██║  ██╗╚██████╔╝\n" +
-                "   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ";
+        String symbol = "+-+-+-+-+-+\n" +
+                "|t|a|l|k|O|\n" +
+                "+-+-+-+-+-+";
         String line = "----------------------------------------------";
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello you can call me");
         System.out.println(symbol);
         System.out.println("How can I help you today?");
         System.out.println(line);
         String input = in.nextLine();
 
-        while(!input.equalsIgnoreCase("bye")) {
-            System.out.println(input);
+        while (!input.equalsIgnoreCase("bye")) {
+            if (input.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println("Yay! You have no tasks :)");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(i+1 + ". " + tasks[i]);
+                    }
+                }
+            } else if (taskCount < tasks.length) {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            } else {
+                System.out.println("talkO has no more room!");
+            }
             System.out.println(line);
             input = in.nextLine();
         }
