8b62977da63ccb9f6adf7ebf915426e9b65a115f
nandhananm7 <nandhanalohithaksh@gmail.com>
2025-02-23 12:32:49 +0530

    Resolved merge conflicts

--- src/main/java/managers/TaskManager.java
+++ src/main/java/managers/TaskManager.java
@@ -4,40 +4,22 @@ import java.util.ArrayList;
 
 import exceptions.TalkoException;
 import tasks.Task;
-import java.util.ArrayList;
 import storage.Storage;
 
 public class TaskManager {
-<<<<<<< HEAD
     private ArrayList<Task> tasks;
     private Storage storage;
+    int taskCount = 0;
 
     public TaskManager() {
-<<<<<<< HEAD
-        this.tasks = new ArrayList<>();
-=======
-    ArrayList<Task> tasks = new ArrayList<>();
-    private int taskCount;
-
-    public TaskManager() {
-        this.taskCount = 0;
->>>>>>> branch-level-6
-=======
         this.storage = new Storage();
         this.tasks = storage.load();
->>>>>>> branch-level-7
     }
 
     public void addTask(Task task) {
         tasks.add(task);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         taskCount++;
->>>>>>> branch-level-6
-=======
         storage.save(tasks);
->>>>>>> branch-level-7
         System.out.println("Added: " + task.getDescription());
     }
 
@@ -45,11 +27,7 @@ public class TaskManager {
         if (tasks.isEmpty()) {
             System.out.println("Yay! You have no tasks :)");
         } else {
-<<<<<<< HEAD
             for (int i = 0; i < tasks.size(); i++) {
-=======
-            for (int i = 0; i < taskCount; i++) {
->>>>>>> branch-level-6
                 System.out.println((i + 1) + ". " + tasks.get(i).getDescription());
             }
         }
@@ -60,23 +38,17 @@ public class TaskManager {
             throw new TalkoException("Please enter a valid task number!");
         } else {
             tasks.get(index).mark(isDone);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
         }
     }
 
     public void removeTask(int index) throws TalkoException {
-        if (index < 0 || index >= taskCount) {
+        if (index < 0 || index >= tasks.size()) {
             throw new TalkoException("Please enter a valid task number!");
         } else {
             System.out.println("Removed: " + tasks.get(index).getDescription());
             tasks.remove(index);
             taskCount--;
->>>>>>> branch-level-6
-=======
             storage.save(tasks);
->>>>>>> branch-level-7
         }
     }
 }
